29265a9964a1e28f9c4aa9d38ee334e0b4690efc
Icaro Sousa <icarosousa131@gmail.com>
2023-01-06 14:33:34 -0300

    Adição de Cliente

---
+++ TransporteV1API/Profiles/ClienteProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using TransporteV1API.Data.Dtos;
+using TransporteV1API.Modals;
+
+namespace TransporteV1API.Profiles
+{
+    public class ClienteProfile : Profile
+    {
+        public ClienteProfile() 
+        {
+            CreateMap<CreateCliente, Cliente>();
+            CreateMap<UpdataCliente, Cliente>();
+        }
+    }
+}
